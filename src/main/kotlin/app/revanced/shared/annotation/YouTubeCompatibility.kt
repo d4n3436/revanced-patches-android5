14155496edd5dbd9739d7008ec8e26f122e29217
d4n3436 <dan3436@hotmail.com>
2023-05-02 14:33:47 -0500

    Allow any YouTube version to be patched and enable protobuf-spoof by default

--- src/main/kotlin/app/revanced/shared/annotation/YouTubeCompatibility.kt
+++ src/main/kotlin/app/revanced/shared/annotation/YouTubeCompatibility.kt
@@ -3,11 +3,7 @@ package app.revanced.shared.annotation
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility(
-    [Package(
-        "com.google.android.youtube", arrayOf("16.40.36", "17.34.36")
-    )]
-)
+@Compatibility([Package("com.google.android.youtube")])
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class YouTubeCompatibility
