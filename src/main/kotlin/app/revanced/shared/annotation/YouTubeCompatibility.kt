6005ce8578e03ef081b4749b9a92966a40ee2160
d4n3436 <dan3436@hotmail.com>
2023-08-06 01:08:38 -0500

    Fix changes introduced after merge

--- src/main/kotlin/app/revanced/shared/annotation/YouTubeCompatibility.kt
+++ src/main/kotlin/app/revanced/shared/annotation/YouTubeCompatibility.kt
@@ -3,7 +3,11 @@ package app.revanced.shared.annotation
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.google.android.youtube")])
+@Compatibility(
+    [Package(
+        "com.google.android.youtube", arrayOf("16.40.36")
+    )]
+)
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class YouTubeCompatibility
