07876d3112ded11fc15151e269d2e1098342828f
d4n3436 <dan3436@hotmail.com>
2023-10-28 22:29:13 -0500

    Include `settings` on `disable-update-screen` patch

--- src/main/kotlin/app/revanced/patches/youtube/misc/updatescreen/resource/patch/UpdateScreenPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/updatescreen/resource/patch/UpdateScreenPatch.kt
@@ -6,6 +6,7 @@ import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
 import app.revanced.patches.youtube.misc.updatescreen.bytecode.patch.UpdateScreenBytecodePatch
 import app.revanced.shared.annotation.YouTubeCompatibility
 import app.revanced.shared.util.resources.ResourceHelper
@@ -13,7 +14,12 @@ import app.revanced.shared.util.resources.ResourceHelper
 @Patch
 @Name("disable-update-screen")
 @Description("Disables the app update screen.")
-@DependsOn([UpdateScreenBytecodePatch::class])
+@DependsOn(
+    [
+        UpdateScreenBytecodePatch::class,
+        SettingsPatch::class
+    ]
+)
 @YouTubeCompatibility
 class UpdateScreenPatch : ResourcePatch {
     override fun execute(context: ResourceContext) {
